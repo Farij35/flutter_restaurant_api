e84c63139183b727a9d69935e7b78e51701cd393
Farij35 <farijaltafsyah60@gmail.com>
2022-07-02 13:30:57 +0700

    fix all

--- lib/data/model/restaurant.dart
+++ lib/data/model/restaurant.dart
@@ -38,6 +38,42 @@ class RestaurantList extends Equatable {
       };
 }
 
+class Restaurant {
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+  });
+
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  double rating;
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
+    id: json["id"],
+    name: json["name"],
+    description: json["description"],
+    pictureId: json["pictureId"],
+    city: json["city"],
+    rating: json["rating"].toDouble(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "description": description,
+    "pictureId": pictureId,
+    "city": city,
+    "rating": rating,
+  };
+}
+
 class RestaurantSearch {
   RestaurantSearch({
     required this.error,
@@ -85,42 +121,6 @@ class RestaurantDetail {
         "error": error,
         "message": message,
         "restaurants": restaurant.toJson(),
-      };
-}
-
-class Restaurant {
-  Restaurant({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.pictureId,
-    required this.city,
-    required this.rating,
-  });
-
-  String id;
-  String name;
-  String description;
-  String pictureId;
-  String city;
-  double rating;
-
-  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
-        id: json["id"],
-        name: json["name"],
-        description: json["description"],
-        pictureId: json["pictureId"],
-        city: json["city"],
-        rating: json["rating"].toDouble(),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "description": description,
-        "pictureId": pictureId,
-        "city": city,
-        "rating": rating,
       };
 }
 
